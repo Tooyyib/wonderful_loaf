9150796ee776a50d1a1d54f6a3c5fd5d16dffd99
Tooyyib <adebisitooyyib@gmail.com>
2026-03-11 23:17:25 +0100

    created text and image demo

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex justify-center'>Home</div>
+    <>
+    <div className='food h-screen flex justify-center'>
+        <div className=' text-4xl font-extrabold text-white'>wonderful Loaf</div>
+    </div>
+
+
+    <div className='bg-yellow-600 text-white'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quod temporibus quaerat ullam expedita provident? Dolore debitis eum beatae incidunt voluptate quae eaque repellendus, nesciunt culpa sed alias accusantium recusandae!
+        Aut harum id voluptas accusantium? Vel dignissimos sunt eligendi minima. Quo non molestiae id consequatur, recusandae facilis saepe, in omnis commodi veniam deleniti eaque reprehenderit neque molestias aut suscipit possimus.
+        Blanditiis soluta libero aperiam dicta inventore nesciunt, laudantium, dolor numquam fuga optio voluptatum perspiciatis architecto hic, amet ullam. Excepturi neque vero magnam sit dolor, explicabo totam aliquid itaque repellendus vel.
+        Quos autem distinctio pariatur soluta voluptatem voluptatum similique, reprehenderit laborum nemo quisquam at reiciendis molestias sequi minus ex sapiente fugit tempore, accusamus porro voluptas dicta ut. Porro reiciendis ducimus voluptatem.
+        Amet quidem incidunt reprehenderit, sequi rem soluta libero, repudiandae inventore debitis recusandae consequuntur mollitia pariatur rerum similique? Quibusdam sapiente, doloribus voluptatibus illo veniam sequi ipsum quaerat, cum perferendis eligendi iusto.
+        Veritatis, aliquid alias facilis, eveniet fugiat perferendis saepe aspernatur laudantium hic sapiente iste? Consequatur unde minima quidem nobis facilis. Corporis sint cum harum eveniet illo reprehenderit at saepe ut tempora.
+        Eveniet sit odit harum animi cumque ipsa, vitae quo ab illum, eius vel ut ea! Voluptatum illum ea explicabo. Cupiditate adipisci voluptatem culpa harum fuga consectetur iure quod corrupti quaerat?
+        Sunt ab velit officia obcaecati eos fugit est laudantium nemo porro, voluptatum placeat neque nam accusamus sint necessitatibus architecto deleniti excepturi dicta dolor esse. Temporibus dolorum quas debitis? Omnis, quo.
+        Maxime error suscipit eos, distinctio deleniti quis perspiciatis illum iure esse quibusdam officia nisi itaque voluptatum inventore architecto eaque consequatur veritatis aperiam, dolor consequuntur labore fugit? Unde placeat ipsa nostrum?
+        Ad reprehenderit omnis aliquid. Esse eveniet non voluptates commodi, sequi voluptatibus quam repellendus distinctio nam sed quo, hic aliquam! Cupiditate excepturi tempora modi assumenda corrupti, quasi esse unde veritatis a.
+        Amet impedit est fugit nulla natus quam, et dolorem voluptatibus quo debitis blanditiis! Voluptatibus, odio facere laudantium voluptatum ipsum quae commodi vel itaque cumque repellendus vero ducimus doloribus voluptatem! Accusantium.
+        Repudiandae, omnis dolorem aut ea animi error eos cumque officiis perspiciatis? Velit, rem recusandae voluptates necessitatibus corrupti amet ratione consequuntur ab sed, culpa nobis! Quos deserunt dignissimos ut quam debitis.
+        Nulla excepturi blanditiis reiciendis dolores iure quod inventore ab quidem ad eveniet dignissimos error rem aliquam veritatis quae modi harum voluptates, repudiandae reprehenderit nesciunt eum animi omnis itaque quis! Rerum!
+        Deleniti recusandae amet soluta libero, porro, inventore aliquid nam velit adipisci aspernatur asperiores, ea illum necessitatibus eos blanditiis? Tempora beatae et corrupti laboriosam explicabo officiis magni harum fuga iste dolorem.
+        Non nam dolore, voluptates laudantium corrupti consequatur necessitatibus quo deserunt itaque, aliquam perspiciatis atque illo voluptatum, quasi amet nesciunt tenetur temporibus sit expedita! Rem at odio officia tenetur ad fuga.
+        Aut excepturi consequatur, ex saepe voluptatem reprehenderit! Nihil, laboriosam qui. Odit dolorem nesciunt alias voluptatem necessitatibus sed esse enim obcaecati reiciendis doloribus beatae, porro quibusdam iure in dignissimos, repudiandae aperiam!
+        Beatae adipisci fugit repudiandae. Inventore veritatis fugit omnis perferendis cumque praesentium obcaecati dolorum earum consequatur laborum, laboriosam quae, nesciunt ad officia illum tempore. Officiis tenetur sint ducimus eligendi in animi!
+        Vel modi, porro quidem ab mollitia aperiam, quisquam optio pariatur rem facilis, eaque ipsa tempora distinctio dolores aspernatur velit recusandae esse! Est voluptatem dolorum dolore adipisci error? Quibusdam, error recusandae.
+        Quibusdam et tempore adipisci excepturi aperiam dolorum ullam voluptas, iusto atque, nesciunt tenetur! Nam, fuga. Praesentium impedit rerum nulla eum ipsa ratione consequatur quaerat nesciunt consequuntur natus, voluptates soluta illo?
+        Deleniti doloremque illo, vero dolorem corrupti suscipit magni unde tempore quia ratione laboriosam animi! Voluptatibus nulla facere ratione sit aut? Accusantium ratione impedit voluptas odio quisquam nostrum dolor minus itaque!
+    </div>
+    </>
+    
   )
 }
 
